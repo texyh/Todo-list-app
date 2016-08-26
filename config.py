0162473392b2d8005c87e62152ebd9dfae2ac286
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2016-08-26 23:27:18 +0100

    database url

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = 'this is no secret'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'main.sqlite')
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	#password hash
 	SECURITY_PASSWORD_SALT = 'my_precious_two'
